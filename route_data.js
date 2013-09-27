40354cb0de6f011261a66ab1b3e99c4cd126f1f0
Nicolas McCurdy <thenickperson@gmail.com>
2013-09-27 03:53:18 -0400

    Move code out of the init function

--- route_data.js
+++ route_data.js
@@ -1,14 +1,10 @@
 "use strict";
 
-// TODO: This only needs to be run once. It might be a good idea to ensure
-// this programatically.
-function init() {
-  // Read in JSON data.
-  // TODO: Get data from schedule.json.
-  var Routes = {};
+// Read in JSON data.
+// TODO: Get data from schedule.json.
+var Routes = {};
 
-  // TODO: Execute algorithm.js.
-}
+// TODO: Execute algorithm.js.
 
 // Utility methods
 
