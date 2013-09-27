e1af0203e8fab1bf0a97cbc93247173ca04608cd
Nicolas McCurdy <thenickperson@gmail.com>
2013-09-27 15:54:29 -0400

    Add a todo comment regarding the creation of a RouteData module

--- route_data.js
+++ route_data.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// TODO: Consider moving this stuff into a RouteData module.
+
 // Read in JSON data.
 // TODO: Get data from schedule.json.
 var Routes = {};
--- to_port/BBRouteData.m
+++ to_port/BBRouteData.m
@@ -6,14 +6,6 @@
 //  Copyright (c) 2013 Altece. All rights reserved.
 //
 
-#import "BBRouteData.h"
-#import "NSArray+Functional.h"
-
-@interface BBRouteData ()
 @property (retain, readwrite, nonatomic) NSDictionary *data;
 @property (retain, readwrite, nonatomic) NSArray *stops;
 @property (retain, readwrite, nonatomic) NSArray *routes;
-@end
-
-@implementation BBRouteData
-@end
