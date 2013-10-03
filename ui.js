d1192553ffd7e3d6a6f6b7d66d21930145a87a65
Nicolas McCurdy <thenickperson@gmail.com>
2013-10-03 02:17:11 -0400

    Add select elements for the source and destination pickers

--- ui.js
+++ ui.js
@@ -1,9 +1,11 @@
 $.getJSON("schedule.json", function (data) {
     console.log(data);
-    Routes = data;
-    console.log(Routes.stops());
-    console.log(Routes.routes());
+    Routes.data = data;
 
+    $.each(Routes.stops(), function (index, stop) {
+        $("#source-picker").append("<option>" + stop + "</option>");
+        $("#destination-picker").append("<option>" + stop + "</option>");
+    });
 })
     .fail(function () {
         console.log("An error has occured in loading schedule.json.");
