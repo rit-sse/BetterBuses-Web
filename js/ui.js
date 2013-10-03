9b45e567be338471100b8fd97e0921635a4594a0
Nicolas McCurdy <thenickperson@gmail.com>
2013-10-03 16:39:32 -0400

    Use angular to populate the select elements with mock data

--- js/ui.js
+++ js/ui.js
@@ -1,12 +1,22 @@
+/*
 $.getJSON("data/schedule.json", function (data) {
     console.log(data);
     Routes.data = data;
 
-    $.each(Routes.stops(), function (index, stop) {
-        $("#source-picker").append("<option>" + stop + "</option>");
-        $("#destination-picker").append("<option>" + stop + "</option>");
-    });
-})
-    .fail(function () {
-        console.log("An error has occured in loading schedule.json.");
-    });
+    // TODO: Set properties of $scope here.
+}).fail(function () {
+    console.log("An error has occured in loading schedule.json.");
+});
+*/
+
+function SourceListCtrl($scope) {
+    $scope.sources = ["Colony Manor", "Gleason Circle"];
+}
+
+function DestinationListCtrl($scope) {
+    $scope.destinations = ["Colony Manor", "Gleason Circle"];
+}
+
+var BetterBusesApp = angular.module('BetterBusesApp', []);
+BetterBusesApp.controller('SourceListCtrl', SourceListCtrl);
+BetterBusesApp.controller('DestinationListCtrl', DestinationListCtrl);
