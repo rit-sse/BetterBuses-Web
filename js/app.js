16ef70929ed09c5ab2f66d7f2b6741bab95128c0
Nicolas McCurdy <thenickperson@gmail.com>
2013-10-21 13:16:44 -0400

    Remove a console.log() call

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@ angular.module("BetterBusesApp", [])
     .controller("StopListController", function ($scope, $http) {
         $scope.loadState = "loading";
         $http.get("data/schedule.json").success(function (data) {
-            console.log(data);
             Routes.data = data;
 
             $scope.stops = Routes.stops();
