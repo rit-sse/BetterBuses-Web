71519de0925353208602e990ebbfd83370715b4a
Nicolas McCurdy <thenickperson@gmail.com>
2013-10-20 22:18:36 -0400

    Add specs for the rest of the algorithm methods

--- spec/RoutesSpec.js
+++ spec/RoutesSpec.js
@@ -29,4 +29,80 @@ describe("Routes", function() {
         expect(Routes.stopsReachableFromStop("Three")).toEqual(["One", "Two", "Four"].sort());
         expect(Routes.stopsReachableFromStop("Four")).toEqual(["Two", "Three"].sort());
     });
+
+    it("gets the schedule for a route", function () {
+        expect(Routes.scheduleForRoute("Route A", "One", "Three", "Monday")).toEqual([[
+            {
+                departs : { to : 'Two', time : '8:00', days : [ 'Monday' ] },
+                arrives : { from : 'One', time : '8:10', days : [ 'Monday' ] },
+                route : 'Route A'
+            }, {
+                departs : { to : 'Three', time : '8:10', days : [ 'Monday' ] },
+                arrives : { from : 'Two', time : '8:20', days : [ 'Monday' ] },
+                route : 'Route A'
+            }
+        ]]);
+
+        expect(Routes.scheduleForRoute("Route B", "One", "Four", "Monday")).toEqual([]);
+        expect(Routes.scheduleForRoute("Route B", "Two", "Three", "Tuesday")).toEqual([]);
+
+        expect(Routes.scheduleForRoute("Route A", "Fake", "Fake", "Monday")).toEqual([]);
+        expect(Routes.scheduleForRoute("Fake", "Two", "Three", "Monday")).toEqual([]);
+        expect(Routes.scheduleForRoute("Fake", "Fake", "Fake", "Monday")).toEqual([]);
+    });
+
+    it("gets the schedules for multiple routes", function () {
+        expect(Routes.schedulesForRoutes(["Route A", "Route B"], "One", "Two", "Monday")).toEqual({
+            "Route A": [[{
+                departs : { to : 'Two', time : '8:00', days : [ 'Monday' ] },
+                arrives : { from : 'One', time : '8:10', days : [ 'Monday' ] },
+                route : 'Route A'
+            }]],
+            "Route B": []
+        });
+
+        expect(Routes.schedulesForRoutes(["Route A", "Route B"], "Two", "Three", "Monday")).toEqual({
+            "Route A": [[{
+                departs : { to : 'Three', time : '8:10', days : [ 'Monday' ] },
+                arrives : { from : 'Two', time : '8:20', days : [ 'Monday' ] },
+                route : 'Route A'
+            }]],
+            "Route B": [[{
+                departs : { to : 'Three', time : '8:00P', days : [ 'Monday' ] },
+                arrives : { from : 'Two', time : '8:10P', days : [ 'Monday' ] },
+                route : 'Route B'
+            }]]
+        });
+
+        expect(Routes.schedulesForRoutes(["Route A", "Route B"], "Two", "Three", "Tuesday")).toEqual({
+            "Route A": [],
+            "Route B": []
+        });
+    });
+
+    it("gets the schedules for any routes starting at a specific stop", function () {
+        expect(Routes.routeSchedulesFromStop("One", "Two", "Monday")).toEqual({
+            "Route A": [[{
+                departs : { to : 'Two', time : '8:00', days : [ 'Monday' ] },
+                arrives : { from : 'One', time : '8:10', days : [ 'Monday' ] },
+                route : 'Route A'
+            }]]
+        });
+    });
+
+    it("gets the schedules for any routes starting at a specific stop, sorted by departure time", function () {
+        expect(Routes.timeSortedSchedulesFromStop("Two", "Three",
+                "Monday")).toEqual([
+            [{
+                departs : { to : 'Three', time : '8:10', days : [ 'Monday' ] },
+                arrives : { from : 'Two', time : '8:20', days : [ 'Monday' ] },
+                route : 'Route A'
+            }],
+            [{
+                departs : { to : 'Three', time : '8:00P', days : [ 'Monday' ] },
+                arrives : { from : 'Two', time : '8:10P', days : [ 'Monday' ] },
+                route : 'Route B'
+            }]
+        ]);
+    });
 });
